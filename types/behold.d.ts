af21188a6426ba889373f2b1fd40e61b37c971db
Esvin Joshua <Joshua.Esvin312@gmail.com>
2026-08-13 19:48:35 +0530

    refactor: move global behold-widget type definitions to a dedicated types file

---
+++ types/behold.d.ts
@@ -0,0 +1,14 @@
+import React, { JSX } from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'behold-widget': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          'feed-id': string;
+        },
+        HTMLElement
+      >;
+    }
+  }
+}
